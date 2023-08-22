eb465d08db3c8c76c9882efbd770884d51c7ee62
Maverick <maverick@Mavericks-MacBook-Air.local>
2023-08-23 00:08:08 +0530

    added project structure

---
+++ src/main/java/com/zero/BookMyShow/BookMyShow/BookMyShowApplication.java
@@ -0,0 +1,13 @@
+package com.zero.BookMyShow.BookMyShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class gitBookMyShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero/BookMyShow/BookMyShow/BookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.BookMyShow.BookMyShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
